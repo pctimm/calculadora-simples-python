1b03ee90b7a8ab8160ac1a2971fd73d0eef6a358
Patrick Timm <86640279+pctimm@users.noreply.github.com>
2021-07-08 23:37:40 -0300

    Simplificando código - print para o input

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # lógica básica
 
 while True:  # executa infinitamente
-    print("Digite o primeiro número")
-    num = float(input())
-    print("Digite o operador matemático")
-    operacao = input()
-    print("Digite outro número")
-    num2 = float(input())
+    num = float(input("Digite o primeiro número"))
+    # debug print(type(num))
+    operacao = input("Digite o operador matemático")
+    # debug print(type(operacao))
+    num2 = float(input("Digite outro número"))
+    # debug print(type(num2))
     if operacao == "+":
         resultado = num + num2
     elif operacao == "-":
@@ -17,8 +17,6 @@ while True:  # executa infinitamente
         resultado = num * num2
     elif operacao == "/":
         resultado = num / num2
-    print(type(resultado))
+
+    # debug print(type(resultado))
     print("Resposta: {:.1f}".format(resultado))
-
-
-
