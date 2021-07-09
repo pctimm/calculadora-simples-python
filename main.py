2a3cade9261c72b9f340b19333db3dc44926ca98
Patrick Timm <86640279+pctimm@users.noreply.github.com>
2021-07-09 00:06:25 -0300

    Começando a trabalhar com a GUI
    nova branch: GUI-init

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-# from tkinter import *  # importa tudo da biblioteca tkinter
+from tkinter import *  # importa tudo da biblioteca tkinter
 
-# lógica básica
+janela = Tk()
+janela.title("Calculadora")
+janela.geometry("500x450")
+text = Text(janela, width=1, height=1)
+text.pack()
+janela.mainloop()
 
-while True:  # executa infinitamente
-    num = float(input("Digite o primeiro número"))
-    # debug print(type(num))
-    operacao = input("Digite o operador matemático")
-    # debug print(type(operacao))
-    num2 = float(input("Digite outro número"))
-    # debug print(type(num2))
-    if operacao == "+":
-        resultado = num + num2
-    elif operacao == "-":
-        resultado = num - num2
-    elif operacao == "*":
-        resultado = num * num2
-    elif operacao == "/":
-        resultado = num / num2
+#while True:  # executa infinitamente
+#    num = float(input("Digite o primeiro número"))
+#    # debug print(type(num))
+#    operacao = input("Digite o operador matemático")
+#    # debug print(type(operacao))
+#    num2 = float(input("Digite outro número"))
+#    # debug print(type(num2))
+#    if operacao == "+":
+#        resultado = num + num2
+#    elif operacao == "-":
+#        resultado = num - num2
+#    elif operacao == "*":
+#        resultado = num * num2
+#    elif operacao == "/":
+#        resultado = num / num2
 
     # debug print(type(resultado))
-    print("Resposta: {:.1f}".format(resultado))
+#    print("Resposta: {:.1f}".format(resultado))
+
+
+
+
